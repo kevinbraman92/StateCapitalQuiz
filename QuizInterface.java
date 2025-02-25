56647ab4c22446f4c991631c829e21255ab58bfa
kevinbraman92 <128657573+kevinbraman92@users.noreply.github.com>
2025-02-25 12:00:37 -0600

    Update QuizInterface.java - Code Cleanup
    Cleaned up the code, making public methods private, changed the programs exit, rearranged methods.

--- QuizInterface.java
+++ QuizInterface.java
@@ -11,7 +11,8 @@ public class QuizInterface {
     private ArrayList<String> stringStorage = new ArrayList<String>();
     private Map<String, State> stateTreeMap = new TreeMap<>();
 
-    public QuizInterface() {}
+    public QuizInterface() {
+    }
 
     public void runQuiz() {
         this.readFile();
@@ -19,46 +20,10 @@ public class QuizInterface {
         this.quiz();
     }
 
-    public void readFile() {
-        try {
-            File myObj = new File("states.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                this.stringStorage.add(data);
-                if (stringStorage.size() == 3) {
-                    State state = new State(stringStorage.get(1), stringStorage.get(0), stringStorage.get(2));
-                    this.stateTreeMap.put(state.getAbbr(), state);
-                    this.stringStorage.clear();
-                }
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("states.txt not found! Please ensure the text file is in the same directory as the Java files!");
-            System.out.println("Exiting program...");
-            System.exit(1);
-        }
-    }
-
-    public void getStates() {
-        if (this.stringStorage.size() != 0) {
-            this.stringStorage.clear();
-        }
-        for (String key : stateTreeMap.keySet()) {
-            this.stringStorage.add(stateTreeMap.get(key).getAbbr());
-        }
-
-    }
-
-    public int randomNumber() {
-        Random randInt = new Random();
-        return randInt.nextInt(this.stateTreeMap.size());
-    }
-
-    public void quiz() {
+    private void quiz() {
         int correctGuesses = 0;
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Welcome to the state capital quiz!");
+        System.out.println("\nWelcome to the state capital quiz!");
         System.out.println("This quiz will pick a random state and ask you to input the capital! Good luck!");
         System.out.println("Enter 'Exit' at any time to end the quiz.");
         while (true) {
@@ -68,7 +33,8 @@ public class QuizInterface {
             String input = userInput.nextLine();
             if (input.equalsIgnoreCase("exit")) {
                 System.out.println("\nExiting quiz, you got " + correctGuesses + " state capitals right!");
-                break;
+                userInput.close();
+                System.exit(0);
             } else if (input.equalsIgnoreCase(this.stateTreeMap.get(randomState).getCapital())) {
                 System.out.println("Good job, you got it!");
                 correctGuesses++;
@@ -77,6 +43,42 @@ public class QuizInterface {
                         + this.stateTreeMap.get(randomState).getCapital());
             }
         }
-        userInput.close();
+    }
+
+    private void readFile() {
+        try {
+            File myObj = new File("states.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                this.stringStorage.add(data);
+                if (this.stringStorage.size() == 3) {
+                    State state = new State(stringStorage.get(1), stringStorage.get(0), stringStorage.get(2));
+                    this.stateTreeMap.put(state.getAbbr(), state);
+                    this.stringStorage.clear();
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(
+                    "states.txt not found! Please ensure the text file is in the same directory as the Java files!");
+            System.out.println(
+                    "Exiting program...");
+            System.exit(1);
+        }
+    }
+
+    private void getStates() {
+        if (this.stringStorage.size() != 0) {
+            this.stringStorage.clear();
+        }
+        for (String key : this.stateTreeMap.keySet()) {
+            this.stringStorage.add(this.stateTreeMap.get(key).getAbbr());
+        }
+    }
+
+    private int randomNumber() {
+        Random randInt = new Random();
+        return randInt.nextInt(this.stateTreeMap.size());
     }
 }
